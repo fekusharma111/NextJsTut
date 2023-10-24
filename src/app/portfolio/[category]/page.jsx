2ac299ee3759de96fbfd25a43f8107d55907b05a
Feku_Sharma <fekusharma111@gmail.com>
2023-10-24 20:19:07 +0530

    DB connection

--- src/app/portfolio/[category]/page.jsx
+++ src/app/portfolio/[category]/page.jsx
@@ -2,27 +2,34 @@ import React from "react";
 import styles from "./page.module.css";
 import Button from "@/components/Button/Button";
 import Image from "next/image";
+import { items } from "./data";
+import { notFound } from "next/navigation";
+
+const getData = (cat) => {
+  const data = items[cat];
+  if (data) {
+    return data;
+  }
+  return notFound();
+};
 
 const Category = ({ params }) => {
-  console.log("params", params);
+  const data = getData(params.category);
   return (
     <div className={styles.container}>
       <h1 className={styles.cardTitle}>{params.category}</h1>
-      <div className={styles.item}>
-        <div className={styles.content}>
-          <h1 className={styles.title}> Test</h1>
-          <p className={styles.desc}>Desc</p>
-          <Button text="See More" url="#" />
+      {data.map((item) => (
+        <div className={styles.item} key={item.id}>
+          <div className={styles.content}>
+            <h1 className={styles.title}> {item.title}</h1>
+            <p className={styles.desc}>{item.desc}</p>
+            <Button text="See More" url="#" />
+          </div>
+          <div className={styles.imgContainer}>
+            <Image src={item.image} alt="img" fill={true} className={styles.img} />
+          </div>
         </div>
-        <div className={styles.imgContainer}>
-          <Image
-            src="https://images.pexels.com/photos/18762544/pexels-photo-18762544/free-photo-of-wraped-in-yellow.jpeg"
-            alt="img"
-            fill={true}
-            className={styles.img}
-          />
-        </div>
-      </div>
+      ))}
     </div>
   );
 };
